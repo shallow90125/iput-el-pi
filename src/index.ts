84d76251c34842bfedb5a55060003bdd47c099a0
shallow90125 <shallow90125@gmail.com>
2023-10-16 14:24:47 +0900

    add server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-(async () => {
+import * as routes from "@/routes";
+import { config } from "@/utils";
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { logger } from "hono/logger";
 
-})();
+const app = new Hono({});
+
+app.use("*", logger());
+app.onError((error, c) => {
+  console.error(error);
+  return c.text(error.message, 500);
+});
+
+(Object.keys(routes) as (keyof typeof routes)[]).map((key) =>
+  app.route("/", routes[key]),
+);
+
+serve({ ...app, hostname: config.address, port: config.port }, (info) => {
+  console.log(`Listening on http://${info.address}:${info.port}`);
+});
