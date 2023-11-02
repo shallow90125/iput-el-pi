3757fe23299e0c30c29237487894dadef4204c98
shallow90125 <shallow90125@gmail.com>
2023-11-02 14:14:42 +0900

    add id-json

--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,62 @@
-import fs from "fs";
+import { readFile, writeFile } from "fs/promises";
+import { ObjectId } from "mongodb";
 import { requestGPIOAccess } from "node-web-gpio";
 import { SHT31 } from "sht31-node";
 import { config } from "./utils/config";
 import { mqtt } from "./utils/mqtt";
-console.log("index.ts");
-const client = mqtt.connect();
 
-client.subscribe("indexID");
-client.on("connect", () => {
-  client.subscribe("alarmStart");
-  client.subscribe("alarmStop");
-  client.subscribe("temperature");
-});
+(async () => {
+  console.log("index.ts");
+  const client = mqtt.connect();
+  let id: string;
 
-client.on("message", async (topic, message) => {
-  const gpioAccess = await requestGPIOAccess();
-  const sensor = gpioAccess.ports.get(config.port.buzzer);
-  if (sensor) {
-    await sensor.export("out");
-  } else {
-    console.error("error");
+  try {
+    const data = await readFile(`id.json`, "utf8");
+    id = JSON.parse(data).id;
+  } catch (e) {
+    id = new ObjectId().toString();
+    await writeFile("id.json", JSON.stringify({ id: id })).catch(() => {});
   }
 
-  if (!sensor) {
-    console.log("tunagattemasen");
-    return;
-  }
-  const id = message.toString();
-  fs.writeFile(`ID.txt`, JSON.stringify({ id: id }), { flag: "a+" }, (err) => {
-    if (err) {
-      console.log(err);
+  client.publish("server", JSON.stringify({ piId: id }));
+
+  client.on("message", async (topic, message) => {
+    console.log(`${topic}: ${JSON.stringify(message.toString())}`);
+    if (topic === `set/${id}`) {
+      console.log(message);
+      console.log("ok!");
+    }
+    const gpioAccess = await requestGPIOAccess();
+    const sensor = gpioAccess.ports.get(config.port.buzzer);
+    if (sensor) {
+      await sensor.export("out");
+    } else {
+      console.error("error");
+    }
+    if (!sensor) {
+      console.log("tunagattemasen");
       return;
     }
-    console.log(`ID:${id} saved to file`);
-    client.publish("ID", `${id}`);
-  });
 
-  if (topic === "alarmStart") {
     console.log("Alarm Start");
     sensor.write(1);
-  }
-  if (topic === "alarmStop") {
+
     console.log("Alarm stop");
     sensor.write(0);
-  }
-  const a = setInterval(async () => {
-    if (topic === "temperature") {
+
+    const a = setInterval(async () => {
       const sht = new SHT31();
       const data = await sht.readSensorData();
       data.temperature = Math.round(data.temperature * 10) / 10;
       data.humidity = Math.round(data.humidity * 10) / 10;
+      client.publish("set/", `${data.temperature}`);
       console.log("温度：", data.temperature, "湿度：", data.humidity);
       if (data.temperature >= 28) {
         console.log("Alarm Stop");
         sensor.write(0);
-        await client.publishAsync("111", "Temperature 28° degrees alarm stop");
+        client.publish("set/", "Temperature 28° degrees alarm stop");
         clearInterval(a);
       }
-    }
-  }, 5000);
-});
-
-// client.on("connect", function () {
-//   const alarm = {
-//     hour: null,
-//     minute: null,
-//     dayOfWeek: [],
-//     isEnabled: null,
-//     timezone: null,
-//   };
-
-//   client.on("message", function (topic, message) {
-//     if (topic === "alarm/data") {
-//       const data = JSON.parse(message.toString());
-//       alarm.hour = data.hour;
-//       alarm.minute = data.minute;
-//       alarm.dayOfWeek = data.dayOfWeek;
-//       alarm.isEnabled = data.isEnabled;
-//       alarm.timezone = data.timezone;
-//     }
-//   });
-//   client.publish("alarm/data", JSON.stringify(alarm));
-// });
+    }, 1000);
+  });
+})();
